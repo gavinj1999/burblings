3ada8bd0bd37513678044d4b06b89bcfdf310227
gavinj1999 <gavinj2006@googlemail.com>
2024-03-13 06:52:43 +0000

    welcome update

--- resources/js/Components/Welcome.vue
+++ resources/js/Components/Welcome.vue
@@ -51,7 +51,7 @@ import ApplicationLogo from '@/Components/ApplicationLogo.vue';
                         <path stroke-linecap="round" d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" />
                     </svg>
                     <h2 class="ms-3 text-xl font-semibold text-gray-900 dark:text-white">
-                        <a href="https://laracasts.com">Laracasts</a>
+                        <a href="https://laracasts.com">Laracasts...</a>
                     </h2>
                 </div>
 
